7bac89e22191d86ea47f8a76093ee377a5759807
Kevin Old <kevin@kevinold.com>
2022-08-05 11:28:45 -0500

    Auth.currentAuthenticatedUser from static page

--- pages/static-page.js
+++ pages/static-page.js
@@ -1,16 +1,29 @@
-import { Amplify, Analytics } from "aws-amplify";
-import { useEffect } from "react";
+import { Amplify, Analytics, Auth } from "aws-amplify";
+import React, { useEffect, useState } from "react";
 import awsExports from "../src/aws-exports";
 
 Amplify.configure({ ...awsExports });
 
 export default function StaticPage() {
+  const [currentUser, setCurrentUser] = useState({});
   useEffect(() => {
     const recordAnalytics = async () => {
       await Analytics.record({ name: "viewStaticPage" });
     };
 
+    Auth.currentAuthenticatedUser().then((user) => {
+      console.log({ user });
+      setCurrentUser(user);
+    });
+
     recordAnalytics().catch(console.error);
   }, []);
-  return <div data-test="content">This is a static page</div>;
+  return (
+    <div data-test="content">
+      This is a static page
+      <code>
+        <pre>{JSON.stringify(currentUser, null, 2)}</pre>
+      </code>
+    </div>
+  );
 }
